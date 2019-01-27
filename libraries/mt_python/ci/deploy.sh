b1c3958bd1bf677017b80079f2cdf2245c4b6953
Justin DuJardin <justin@dujardinconsulting.com>
2019-01-26 18:54:28 -0800

    fix: test new python deploy integration
     - try using the single semantic-release node project that delegates to python for a build step.

--- libraries/mt_python/ci/deploy.sh
+++ libraries/mt_python/ci/deploy.sh
@@ -2,18 +2,33 @@
 set -e
 
 . .env/bin/activate
-
-echo "Prepare pypi configuration..."
-echo -e "[pypi]" >> ~/.pypirc
-echo -e "username = justindujardin" >> ~/.pypirc
-echo -e "password = $PYPI_PASSWORD" >> ~/.pypirc
-
-echo "Build python package..."
-sh ci/build.sh
-
-echo "Publish to pypi..."
 git config --global user.email "justin@dujardinconsulting.com"
 git config --global user.name "justindujardin"
-semantic-release publish
+
+
+NEXT_VERSION=$1
+BRANCH_NAME=$2
+COMMIT_COUNT=$3
+
+echo "---------------- Deploy to PyPi:"
+echo "                 Version: ${NEXT_VERSION}"
+echo "                 Branch : ${BRANCH_NAME}"
+echo "                 Commits: ${COMMIT_COUNT}"
+
+echo "Build and publish to pypi..."
+rm -rf build dist
+virtualenv -p python3.6 .env
+. .env/bin/activate
+echo "--- Install requirements"
+pip install twine wheel
+pip install -r requirements.txt
+echo "--- Buid dists"
+python setup.py sdist bdist_wheel
+echo "--- Upload to PyPi"
+twine upload -u ${PYPI_USERNAME} -p ${PYPI_PASSWORD} dist/*
+rm -rf build dist
+
+
+
 
 echo "TODO: Use https://pypi.org/project/pydoc-markdown/ to export and commit MD files in the docs/python/ folder on builds to develop/master."
